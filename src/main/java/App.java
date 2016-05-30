70dbd8587b9f4c9d1dc2e87632c1f308ec2fb267
Angus Lowe <anguslowe@live.ca>
2016-05-29 20:12:55 -0400

    initial commit

---
+++ src/main/java/App.java
@@ -0,0 +1,13 @@
+import java.util.Map;
+import java.util.HashMap;
+
+import com.sun.org.apache.xpath.internal.operations.Mod;
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+import static spark.Spark.*;
+
+public class App {
+    public static void main(String args[]) {
+
+    }
+}
